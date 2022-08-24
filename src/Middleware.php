13b5adb1137f02dc67945770174d63bddcbf8fa1
i360ss <shakeerwahid76@gmail.com>
2022-08-25 01:21:41 +0530

    Middleware cleanup

--- src/Middleware.php
+++ src/Middleware.php
@@ -7,27 +7,34 @@
 
 namespace Ozz\Core;
 
-use App\RegisterMiddleware;
-
 class Middleware {
   
   public static function execute($middleware = null, $coreData=null){
-    if($middleware == null){
-      $mv = RegisterMiddleware::autoMiddleware();
+    require __DIR__.SPC_BACK['core'].'/app/RegisterMiddleware.php';
+
+    if ($middleware == null) {
+      $mv = $auto_middlewares;
       foreach ($mv as $k => $v) {
-        $callBackMv[0] = new $v($coreData);
-        $callBackMv[1] = 'handle';
-        call_user_func( $callBackMv );
+        if (class_exists($v)) {
+          $callBackMv[0] = new $v($coreData);
+          $callBackMv[1] = 'handle';
+          call_user_func( $callBackMv );
+        } else {
+          return DEBUG 
+          ? Err::custom([
+            'msg' => "Middleware Class [$v] Not found",
+            'info' => 'You have to create the middleware before calling it',
+            'note' => "Run [ php ozz c:middleware $v]",
+          ])
+          : false;
+        }
       }
-    }
-    else{
-      if(array_key_exists($middleware, RegisterMiddleware::routeMiddleware())){
-        $mv = RegisterMiddleware::routeMiddleware();
-        $callBackMv[0] = new $mv[$middleware]($coreData);
+    } else {
+      if (array_key_exists($middleware, $route_middlewares)) {
+        $callBackMv[0] = new $route_middlewares[$middleware]($coreData);
         $callBackMv[1] = 'handle';
         return call_user_func( $callBackMv );
-      }
-      else{
+      } else {
         Err::invalidMiddleware($middleware);
       }
     }
