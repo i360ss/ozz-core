01291a0b230dde0743b37e98c16e5f07947c9940
Shakir Wahid <shakeerwahid@gmail.com>
2022-09-30 00:26:36 +0530

    Ozz Serve - Browser opening fix (OS fix) + env() update

--- src/system/cli/Serve.php
+++ src/system/cli/Serve.php
@@ -14,13 +14,19 @@ class Serve {
     $utils = new CliUtils;
     extract($utils->styles);
     
-    $conf = parse_ini_file(__DIR__.SPC_BACK['core_2'].'env.ini', true);
-    $url = $conf['app']['APP_URL'].':'.$conf['app']['LOCAL_PORT'];
+    $conf = env('app');
+    $url = $conf['APP_URL'].':'.$conf['LOCAL_PORT'];
 
     $utils->console_return('Ozz development server started', 'white', 'green', 1, true);
     $utils->console_return('Press Ctrl+C to stop the server', 'brown', false, 2);
-    exec( sprintf('start %s', 'http://'.$url) ); // Open on default browser
-    exec('php -S '.$url.' -t '.$conf['app']['PUBLIC_DIR']);
+
+    $uname = strtolower(php_uname());
+    $os = (strpos($uname, "darwin") !== false) ? 'osx' : ((strpos($uname, "win") !== false) ? 'win32' : 'linux');
+    $end = $os == 'win32' ? '' : '&';
+    $cmd1 = sprintf("%s http://$url $end", $os == 'win32' ? 'start ""' : ($os == 'osx' ? 'open' : 'xdg-open'));
+    pclose(popen($cmd1, "r"));
+
+    exec('php -S '.$url.' -t '.$conf['PUBLIC_DIR']); // Start Dev Server
     die;
   }
 }
