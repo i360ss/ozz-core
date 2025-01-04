011d9611e1417f2133da08cd1e691fb6043bf497
i360ss <shakeerwahid76@gmail.com>
2025-01-04 15:07:13 +0530

    Assets directory renames to resources and moved out to root

--- src/system/content-holder/cms/as/js/modules/InitOzzWyg.js
+++
@@ -1,7 +0,0 @@
-import OzzWyg from '../vendor/ozz-wyg';
-
-export default () => {
-  if (typeof OzzWyg === 'function') {
-    new OzzWyg({ selector: '[data-ozz-wyg]' });
-  }
-}
--- src/system/content-holder/cms/as/js/modules/RelocatePostInfoComp.js
+++
@@ -1,7 +0,0 @@
-export default () => {
-  const postInfoComp =  document.getElementById('post-info-comp');
-  if (postInfoComp === null) return;
-
-  const postSidebar = document.querySelector('.post-edit-view__sidebar');
-  postSidebar.insertBefore(postInfoComp, postSidebar.firstChild);
-}
